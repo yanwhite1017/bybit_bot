e6fd7760b793e934406d81f976c2fe6dda39bd05
yanwhite1017 <52163207+yanwhite1017@users.noreply.github.com>
2023-02-16 12:52:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -105,11 +105,11 @@ def handle_function(message):
 				#							symbol=poll_symbol_pair,
 				#							qty=qValue)
 
-				Longfloat = float(data_candle[0]["close"])-float(data_candle[0]["close"])/100*0.45
+				Longfloat = float(data_candle[0]["close"])-float(data_candle[0]["close"])/100*0.15
 				short_Longfloat = toFixed(Longfloat, 2)
 				short_Longfloat = float(short_Longfloat)
 
-				tp = float(data_candle[0]["close"])+float(data_candle[0]["close"])/100*0.45
+				tp = float(data_candle[0]["close"])+float(data_candle[0]["close"])/100*0.25
 				short_tp = toFixed(tp, 2)
 				short_tp = float(short_tp)
 
@@ -136,11 +136,11 @@ def handle_function(message):
 				#							secret=secretKey,
 				#							symbol=poll_symbol_pair,
 				#							qty=qValue)
-				Shortfloat = float(data_candle[0]["close"])+float(data_candle[0]["close"])/100*0.45
+				Shortfloat = float(data_candle[0]["close"])+float(data_candle[0]["close"])/100*0.15
 				short_Shortfloat = toFixed(Shortfloat, 2)
 				short_Shortfloat = float(short_Shortfloat)
 
-				tp = float(data_candle[0]["close"])-float(data_candle[0]["close"])/100*0.45
+				tp = float(data_candle[0]["close"])-float(data_candle[0]["close"])/100*0.25
 				short_tp = toFixed(tp, 2)
 				short_tp = float(short_tp)
 
@@ -156,7 +156,7 @@ def handle_function(message):
 
 
 
-ws_perpetual.kline_stream(handle_function, symbols, "15")
+ws_perpetual.kline_stream(handle_function, symbols, "5")
 
 while True:
 	sleep(1)
